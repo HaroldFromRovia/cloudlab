1055d67f9b0952565da5e8e3c3b29674d2f2de7f
hyperbot <vitalij-groshev@mail.ru>
2021-12-26 20:01:28 +0300

    add q-trigger

---
+++ q-trigger/src/main/java/EventParser.java
@@ -0,0 +1,13 @@
+import org.json.JSONObject;
+
+public class EventParser {
+
+    public String getObjectKey(String eventJson) {
+        JSONObject json = new JSONObject(eventJson);
+        return json.getJSONArray("messages")
+                .getJSONObject(0)
+                .getJSONObject("details")
+                .getJSONObject("message")
+                .getString("body");
+    }
+}
---
+++ q-trigger/src/main/java/Handler.java
@@ -0,0 +1,54 @@
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import yandex.cloud.sdk.functions.Context;
+import yandex.cloud.sdk.functions.YcFunction;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class Handler implements YcFunction<String, String> {
+    @Override
+    public String handle(String eventJson, Context context) {
+        String id = System.getenv("aws_access_key_id");
+        String secret = System.getenv("aws_secret_access_key");
+        String chatId = Optional.ofNullable(System.getenv("default_chat_id")).orElse("357098955");
+        String botToken = System.getenv("bot_token");
+        String bucketName = Optional.ofNullable(System.getenv("bucket")).orElse("cloudphoto16");
+        AmazonS3 amazonS3 = null;
+        try {
+            amazonS3 = AmazonS3ClientBuilder.standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(
+                            new BasicAWSCredentials(id, secret)))
+                    .withEndpointConfiguration(
+                            new AmazonS3ClientBuilder.EndpointConfiguration(
+                                    "storage.yandexcloud.net", "ru-central1"
+                            )
+                    )
+                    .build();
+        } catch (Exception e) {
+            System.err.println("can't create s3 instance");
+            e.printStackTrace();
+        }
+
+        EventParser eventParser = new EventParser();
+        String objectKey = eventParser.getObjectKey(eventJson);
+        byte[] photoBytes = null;
+        try {
+             photoBytes = amazonS3.getObject(bucketName, objectKey).getObjectContent().readAllBytes();
+        } catch (IOException e) {
+            System.err.println("can't get object bytes by key from the message (update)");
+        }
+        try {
+            chatId = amazonS3.listObjectsV2(bucketName, "users").getObjectSummaries().get(0).getKey();
+        }
+        catch (Exception e) {
+            System.err.println("can't get chatId from object storage");
+        }
+
+        TelegramSender telegramSender = new TelegramSender(botToken);
+        telegramSender.sendPhoto(chatId, photoBytes, objectKey);
+        return "ok";
+    }
+}
